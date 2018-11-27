5ddca34310b6460f153f767fd0df4a2579e302c0
Vang Xiong <vxiong029@gmail.com>
2018-11-27 16:30:52 -0600

    input field is working/button added and logs info from input fields

--- src/App.js
+++ src/App.js
@@ -2,11 +2,38 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    famousPerson: {
+      name: '',
+      role: ''
+    }
+  }
+  handleChange = (propertyName) => {
+    return (event) => {
+      this.setState({
+        famousPerson: {
+          ...this.state.famousPerson,
+          [propertyName]: event.target.value
+        }
+      })
+    }
+  }
+  handleClick = () => {
+    console.log(this.state);
+  }
   render() {
     return (
-      <header>
-        <h1>Famous People</h1>
-      </header>
+      <div className="App">
+        <header>
+          <h1>Famous People</h1>
+        </header>
+        <section>
+          <input type="text" placeholder="Name" onChange={this.handleChange('name')} />
+          <input type="text" placeholder="Role" onChange={this.handleChange('role')} />
+          <button onClick={this.handleClick}>add me</button>
+          <p>{this.state.famousPerson.name} is famous for {this.state.famousPerson.role}.</p>
+        </section>
+      </div>
     );
   }
 }
